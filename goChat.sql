b95f712f1b30ffc2075ec65b2cb4b0f6cdbf3670
landon <landonr98@gmail.com>
2023-06-08 22:10:51 -0230

    sql update

--- goChat.sql
+++ goChat.sql
@@ -1,44 +1,48 @@
 CREATE DATABASE  IF NOT EXISTS `gochat`;
 USE `gochat`;
 
-
 DROP TABLE IF EXISTS `chat_participants`;
-
 CREATE TABLE `chat_participants` (
   `id` int unsigned NOT NULL AUTO_INCREMENT,
   `user_id` int unsigned NOT NULL,
   `chat_id` int unsigned NOT NULL,
+  `accepted_invite` tinyint NOT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE KEY `id_UNIQUE` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
+  UNIQUE KEY `id_UNIQUE` (`id`),
+  KEY `fk_chat_participants_user_id_idx` (`user_id`),
+  KEY `fk_chat_participants_chat_id_idx` (`chat_id`),
+  CONSTRAINT `fk_chat_participants_chat_id` FOREIGN KEY (`chat_id`) REFERENCES `chats` (`id`),
+  CONSTRAINT `fk_chat_participants_user_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 DROP TABLE IF EXISTS `chats`;
-
 CREATE TABLE `chats` (
   `id` int unsigned NOT NULL AUTO_INCREMENT,
-  `name` varchar(45) DEFAULT NULL,
+  `name` varchar(45) NOT NULL,
   `is_private` tinyint NOT NULL DEFAULT '0',
-  `creator` int DEFAULT NULL,
+  `creator` int unsigned NOT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE KEY `id_UNIQUE` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  UNIQUE KEY `id_UNIQUE` (`id`),
+  KEY `fk_chats_creator_idx` (`creator`),
+  CONSTRAINT `fk_chats_creator` FOREIGN KEY (`creator`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 
 DROP TABLE IF EXISTS `message`;
-
 CREATE TABLE `message` (
   `id` int unsigned NOT NULL AUTO_INCREMENT,
   `chat_id` int unsigned NOT NULL,
   `user_id` int unsigned NOT NULL,
   `message` varchar(200) NOT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE KEY `id_UNIQUE` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
+  UNIQUE KEY `id_UNIQUE` (`id`),
+  KEY `fk_message_chat_id_idx` (`chat_id`),
+  KEY `fk_message_user_id_idx` (`user_id`),
+  CONSTRAINT `fk_message_chat_id` FOREIGN KEY (`chat_id`) REFERENCES `chats` (`id`),
+  CONSTRAINT `fk_message_user_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 DROP TABLE IF EXISTS `users`;
-
 CREATE TABLE `users` (
   `id` int unsigned NOT NULL AUTO_INCREMENT,
   `username` varchar(45) NOT NULL,
@@ -46,5 +50,5 @@ CREATE TABLE `users` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `id_UNIQUE` (`id`),
   UNIQUE KEY `username_UNIQUE` (`username`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
